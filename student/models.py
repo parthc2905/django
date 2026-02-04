a69c26d55f82d0b053fd11f4738166d5ec90c7eb
parthc2905 <chauhanp2905@gmail.com>
2026-02-04 11:15:04 +0530

    Create Student Product Models

--- student/models.py
+++ student/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# student Database
+class Student(models.Model):
+    studentName = models.CharField(max_length=100)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+    #meta class
+    class Meta:
+        db_table = "student"   #table name
+
+#product db
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20, null=True)
+    productStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "product"
